ea34ad59a18cb4b5f9e4406ae3890cd83b16e34b
fullstackwithsahil <fullstackwithsahil@gmail.com>
2026-05-13 11:13:08 +0530

    fresh start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-import Hero from "@/components/landingPage/Hero";
-import Products from "@/components/landingPage/Products";
-import PartnerSection from "@/components/landingPage/Partner";
-import Faq from "@/components/landingPage/Faq";
-import Leadmagnet from "@/components/landingPage/Leadmagnet";
-import TrustSection from "@/components/landingPage/TrustSection";
-import ResourcesCTA from "@/components/landingPage/ResourcesCTA";
+import React from 'react'
 
 export default function page() {
-    return (
-        <main>
-            <Hero />
-            <TrustSection />
-            <PartnerSection />
-            <Products />
-            <ResourcesCTA />
-            <Leadmagnet />
-            <Faq />
-        </main>
-    );
+  return (
+    <div>page</div>
+  )
 }
